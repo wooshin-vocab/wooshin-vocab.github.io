01f0ba801eb43c0c0dc92b7710838d9c2387cf2b
wooshin-vocab <yeppi486@naver.com>
2026-08-24 20:31:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wooshin-vocab-v4-3";
+const CACHE="wooshin-vocab-v4-4";
 const ASSETS=["./","./index.html","./test.html","./words-v41.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())});
